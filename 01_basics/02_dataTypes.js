6906967bd209d121c60791bcff2dddf1d4ed742b
Mir Akib Javed <mirakibjaved22@gmail.com>
2024-09-15 19:00:53 +0000

    data_types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,20 @@
 
 // alert("Ok"); // we're using node js, not browser
 
+let name = "Akib";
+let age = 27;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
